28d6efe88abeca65a1f552f3f48671d3a8f9fa60
Zlash65 <zarrar65@gmail.com>
2018-06-11 16:30:01 +0530

    frappejs connection added, packages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import initFrappe from "../frappe";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,4 +28,8 @@ import { HomePage } from '../pages/home/home';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+	constructor() {
+		initFrappe();
+	};
+}
---
+++ src/frappe.js
@@ -0,0 +1,31 @@
+import frappe from 'frappejs';
+
+import common from 'frappejs/common';
+import models from 'frappejs/models';
+import HTTPClient from 'frappejs/backends/http';
+import io from 'socket.io-client';
+import Observable from 'frappejs/utils/observable';
+
+import fetch from 'node-fetch';
+
+// Enter your ip:8000 here
+let server = 'localhost:8000';
+
+export default async function initFrappe(serv) {
+	if(serv) {
+		server = serv;
+	}
+	Object.assign(window, {
+	  frappe
+	});
+	frappe.fetch = fetch.bind();
+	
+	frappe.init();
+	frappe.registerLibs(common);
+	frappe.registerModels(models, 'client');
+	frappe.db = await new HTTPClient({server: server});
+	const socket = io.connect('http://' + server);
+	frappe.db.bindSocketClient(socket);
+	
+	frappe.docs = new Observable();
+}
